d055d12d0d0a9390ec24f7ee8391a2d751c33ca3
jonas.franke1@gmx.net <12jkinghb@gmail.com>
2022-07-09 22:09:55 +0200

    speed fix?

--- app/src/main/java/de/jo/boattracker/util/SpeedConverter.java
+++ app/src/main/java/de/jo/boattracker/util/SpeedConverter.java
@@ -2,11 +2,11 @@ package de.jo.boattracker.util;
 
 public class SpeedConverter {
 
-    public static double getMetersPerSecondToKnots(double ms) {
-        return ms * 1.943844;
+    public static float getMetersPerSecondToKnots(float ms) {
+        return ms * 1.943844f;
     }
 
-    public static double getKnotsToMetersPerSecond(double kn) {
-        return kn / 1.943844;
+    public static float getKnotsToMetersPerSecond(float kn) {
+        return kn / 1.943844f;
     }
 }
